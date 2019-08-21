f8967ca50b52657028a6dab9c917622a8d5dd1e7
sparsh30cnb <51509096+sparsh30cnb@users.noreply.github.com>
2019-08-21 11:01:21 +0530

    Update test-8–1.conf.js

--- test-8–1.conf.js
+++ test-8–1.conf.js
@@ -1,6 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
-exports.config = {
+  exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js']
+},
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
