713b031f08ba4d36b95d3bc9dc4eac84b0a2051d
sparsh30cnb <51509096+sparsh30cnb@users.noreply.github.com>
2019-08-21 14:09:56 +0530

    jasmine framework added @ line 11

--- protractor-first-test.conf.js
+++ protractor-first-test.conf.js
@@ -1,6 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 exports.config = {
+  framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['todo-spec.js'],
   capabilities: {
