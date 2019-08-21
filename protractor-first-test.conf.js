45e9b876b3616a1a5fdbe86a05005efaa637ee07
sparsh30cnb <51509096+sparsh30cnb@users.noreply.github.com>
2019-08-21 11:08:36 +0530

    Update and rename test-8–1.conf.js to protractor-first-test.conf.js

--- protractor-first-test.conf.js
+++ protractor-first-test.conf.js
@@ -1,9 +1,4 @@
-// Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config.ts
-  exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['todo-spec.js']
-},
+exports.config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
